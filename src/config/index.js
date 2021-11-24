08632b119e836c32198275c854024246fdc7e891
jason-pgb <1841599086@qq.com>
2021-11-24 13:12:19 +0800

    mockapi更改

--- src/config/index.js
+++ src/config/index.js
@@ -9,11 +9,11 @@ const EnvConfig = {
     },
     test: {
         baseApi: '//test.futurefe.com/api',
-        mockApi: 'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+        mockApi: 'https://www.fastmock.site/mock/9496d91c0cdc682b59c51ac1399cd655/api'
     },
     prod: {
         baseApi: '//futurefe.com/api',
-        mockApi: 'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+        mockApi: 'https://www.fastmock.site/mock/9496d91c0cdc682b59c51ac1399cd655/api'
     }
 }
 export default {
